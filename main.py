867218071e25f18c36be9c89eb3bc897695f1ede
Efe Erkan <72245656+EfeErkan@users.noreply.github.com>
2023-03-24 13:02:05 +0300

    Part B initial

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from rna_folding import RNA_Folding_MAX_PAIRS
+from rna_folding import RNA_Folding_MAX_PAIRS, RNA_Folding_MIN_Energy
 
 def main():
     RNA_df = pd.read_excel('data.xlsx')
     
     for index, row in RNA_df.iterrows():
         RNA = row['RNA']
-        print(f"Max pairs Group {index + 1}: {RNA_Folding_MAX_PAIRS(RNA)}")
+        print(f"Min energy Group {index + 1}: {RNA_Folding_MIN_Energy(RNA)}")
 
 if __name__ == '__main__':
     main()
--- rna_folding.py
+++ rna_folding.py
@@ -7,19 +7,19 @@ env.start()
 
 def isComplementary(ch1, ch2):
     if ch1 == 'A' and ch2 == 'U':
-        return True
+        return 1
     elif ch1 == 'U' and ch2 == 'A':
-        return True
+        return 1
     elif ch1 == 'G' and ch2 == 'C':
-        return True
+        return 2
     elif ch1 == 'C' and ch2 == 'G':
-        return True
-    return False
+        return 2
+    return 0
 
 def isValidPairing(RNA:str, i:int, j:int, distance_limit:int = 4):
     if abs(i - j) < distance_limit:
         return False
-    if isComplementary(RNA[i - 1], RNA[j - 1]) == False:
+    if isComplementary(RNA[i - 1], RNA[j - 1]) == 0:
         return False
     return True
 
@@ -35,6 +35,39 @@ def RNA_Folding_MAX_PAIRS(RNA:str, distance_limit:int = 4): # Part A
     
     # Objective function   
     model.setObjective(X.sum(), GRB.MAXIMIZE)
+
+    # At most 1 pairing        
+    for i in range(1, len(RNA) + 1):
+        model.addConstr(quicksum(X[j, i] for j in range(1, i)) + quicksum(X[i, j] for j in range(i + 1, len(RNA) + 1)) <= 1)
+
+    # Compplementary characters => A-U, G-C and distance limitation
+    for i in range(1, len(RNA) + 1):
+        for j in range(i + 1, len(RNA) + 1):
+            if isValidPairing(RNA, i, j, distance_limit) == False:
+                model.addConstr(X[i, j] == 0)
+
+    # No cross pairing            
+    for i in range(1, len(RNA) + 1):
+        for k in range(i + 1, len(RNA) + 1):
+            for j in range(k + 1, len(RNA) + 1):
+                for l in range(j + 1, len(RNA) + 1):
+                    if isValidPairing(RNA, i, j, distance_limit) and isValidPairing(RNA, k, l, distance_limit):
+                        model.addConstr(X[i, j] + X[k, l] <= 1)
+
+    model.optimize()
+    return model.objVal
+
+def RNA_Folding_MIN_Energy(RNA:str, A_U_energy:float = -1.33, G_C_ENERGY:float = -1.45, distance_limit:int = 4): # Part B
+    model = Model("RNA", env=env)
+    X = tupledict()
+
+    # Decision variables
+    for i in range(1, len(RNA) + 1):
+        for j in range(i + 1, len(RNA) + 1):
+            X[i, j] = model.addVar(vtype=GRB.BINARY, name=f"x{i}_{j}")
+    
+    # Objective function   
+    model.setObjective(quicksum(X[i, j] * (A_U_energy if isComplementary(RNA[i - 1], RNA[j - 1]) == 1 else (G_C_ENERGY if isComplementary(RNA[i - 1], RNA[j - 1]) == 2 else 0)) for i in range(1, len(RNA) + 1) for j in range(i + 1, len(RNA) + 1)), GRB.MINIMIZE)
 
     # At most 1 pairing        
     for i in range(1, len(RNA) + 1):
